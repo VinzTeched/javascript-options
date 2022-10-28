9a7a19972870d5130e62cb1689bf529905518b74
Chibundo Ilonze <chibundoilonze@gmail.com>
2022-10-28 06:21:09 +0100

    final sources

--- addFive.js
+++ addFive.js
@@ -2,4 +2,10 @@ function addFive(val) {
     return val + 5;
 }
 
+function statusOfKeys() {
+    //
+    console.log("red")
+}
+
+
 module.exports = addFive;
--- addFive.test.js
+++ addFive.test.js
@@ -1,6 +1,18 @@
 const { default: TestRunner } = require('jest-runner');
 const addFive = require('./addFive');
+const statusOfKeys = require('./addFive');
 
 test('returns the number plus 5', () => {
     expect(addFive(1)).toBe(6)
 })
+
+test('returns true if statusOfKeys exists', function() {
+    expect(statusOfKeys).toBeDefined()
+})
+
+test('check that console.log runs', function() {
+    //statusOfKeys();
+    //expect(console.log).toBeCalled();
+})
+
+
